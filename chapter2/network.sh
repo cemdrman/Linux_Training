dc9ae4acea5c34022ae387c0e4f0bbd080fa3a8b
Cem Dırman <cemdrman@gmail.com>
2019-12-09 22:26:46 +0300

    network & vim editör

---
+++ chapter2/network.sh
@@ -0,0 +1,18 @@
+$ifconfig
+$iwconfig
+
+$ping github.com
+
+$traceroute github.com
+
+$netstat
+
+$nslookup
+
+$whois
+$host github.com
+
+
+#-----SSH
+
+#haberleşme için güvenli bir bağlantı sağlayan protokoldür.
---
+++ chapter2/vim.sh
@@ -0,0 +1,8 @@
+#en bilinen text editörüdür. Konsol üzerinden açılır.
+
+#vim ile açtığınız text dosyasını esc'ye basarak komut olabilir hale getirirsiniz.
+
+:q  çık
+:q! kaydetmeden çık
+:w  kaydet
+:wq kaydet ve çık
